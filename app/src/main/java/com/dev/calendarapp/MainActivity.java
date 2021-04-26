406a8e8dbbf25721f3a0882f36116c5162ff7b96
Ilane-glitch <ilane.bouttemant@gmail.com>
2021-04-26 19:05:54 +0200

    update

--- app/src/main/java/com/dev/calendarapp/MainActivity.java
+++ app/src/main/java/com/dev/calendarapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.dev.calendarapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //lancement de l'activité calendrier
+        Intent intent = new Intent (MainActivity.this, CalendarActivity.class);
+        startActivity(intent);
     }
 }
